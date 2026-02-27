3a0a93dcbd7a47828477c344ad794dad1ba11e1a
dyeisley <dyeisley@redhat.com>
2026-02-27 20:22:12 +0000

    Split the index creation so we could background the processes and run them in parallel.

--- mysqlds35/build/mysqlds35_perl_create_indexes_multi.pl
+++ mysqlds35/build/mysqlds35_perl_create_indexes_multi.pl
@@ -1,6 +1,6 @@
 # mysqlds3_perl_create_indexes_multi.pl
 # Script to create a ds35 indexes in MySQL with a provided number of copies - supporting multiple stores
-# Syntax to run - perl mysqlds3_perl_create_indexes_multi.pl <mysql_target> <number_of_stores> 
+# Syntax to run - perl mysqlds3_perl_create_indexes_multi.pl <mysql_target> <number_of_stores>
 
 use strict;
 use warnings;
@@ -8,10 +8,12 @@ use warnings;
 my $mysqltarget = $ARGV[0];
 my $numberofstores = $ARGV[1];
 
-my $pathsep; 
+my $pathsep;
+my $mybackground;
+my $indexfile;
 
 #Need seperate target directory so that mulitple DB Targets can be loaded at the same time
-my $mysql_targetdir;  
+my $mysql_targetdir;
 
 $mysql_targetdir = $mysqltarget;
 
@@ -24,39 +26,55 @@ system ("mkdir -p $mysql_targetdir");
 if ("$^O" eq "linux")
         {
         $pathsep = "/";
+	$mybackground = "&";
+	#$mybackground = "";
         }
 else
         {
         $pathsep = "\\\\";
+	$mybackground = "";
         };
 
+$indexfile="mysqlds35_create_customer_indexes.sql";
 foreach my $k (1 .. $numberofstores){
-	open (my $OUT, ">$mysql_targetdir${pathsep}mysqlds35_createindexes.sql") || die("Can't open $mysql_targetdir${pathsep}mysqlds35_createindexes.sql");
+	open (my $OUT, ">$mysql_targetdir${pathsep}$indexfile") || die("Can't open $mysql_targetdir${pathsep}$indexfile");
 	print $OUT  "-- Tables
 USE DS3;
 
-CREATE UNIQUE INDEX IX_CUST_USERNAME$k ON CUSTOMERS$k 
+CREATE UNIQUE INDEX IX_CUST_USERNAME$k ON CUSTOMERS$k
   (
   USERNAME
   );
 
-CREATE INDEX IX_CUST_HIST_CUSTOMERID$k ON CUST_HIST$k
-  (
-  CUSTOMERID
-  );
+CREATE INDEX IX_CUST_HIST_CUSTOMERID_PRODID$k ON CUST_HIST$k
+   (
+   CUSTOMERID,
+   PROD_ID
+   );
 
 ALTER TABLE CUST_HIST$k
   ADD CONSTRAINT FK_CUST_HIST_CUSTOMERID$k FOREIGN KEY (CUSTOMERID)
   REFERENCES CUSTOMERS$k (CUSTOMERID)
   ON DELETE CASCADE
   ;
+\n";
+  close $OUT;
+  sleep(1);
+  print ("mysql -h $mysqltarget -u web --password=web < $mysql_targetdir${pathsep}$indexfile\n");
+  system ("mysql -h $mysqltarget -u web --password=web < $mysql_targetdir${pathsep}$indexfile $mybackground");
+  }
 
+$indexfile="mysqlds35_create_orders_indexes.sql";
+foreach my $k (1 .. $numberofstores){
+	open (my $OUT, ">$mysql_targetdir${pathsep}$indexfile") || die("Can't open $mysql_targetdir${pathsep}$indexfile");
+	print $OUT  "-- Tables
+USE DS3;
 CREATE INDEX IX_ORDER_CUSTID$k ON ORDERS$k
   (
   CUSTOMERID
   );
 
-ALTER TABLE ORDERS$k 
+ALTER TABLE ORDERS$k
   ADD CONSTRAINT FK_CUSTOMERID$k FOREIGN KEY (CUSTOMERID)
   REFERENCES CUSTOMERS$k (CUSTOMERID)
   ON DELETE SET NULL
@@ -72,8 +90,19 @@ ALTER TABLE ORDERLINES$k
   REFERENCES ORDERS$k (ORDERID)
   ON DELETE CASCADE
   ;
+\n";
+  close $OUT;
+  sleep(1);
+  print ("mysql -h $mysqltarget -u web --password=web < $mysql_targetdir${pathsep}$indexfile\n");
+  system ("mysql -h $mysqltarget -u web --password=web < $mysql_targetdir${pathsep}$indexfile $mybackground");
+  }
 
-CREATE FULLTEXT INDEX IX_PROD_ACTOR$k ON PRODUCTS$k 
+$indexfile="mysqlds35_create_products_indexes.sql";
+foreach my $k (1 .. $numberofstores){
+	open (my $OUT, ">$mysql_targetdir${pathsep}$indexfile") || die("Can't open $mysql_targetdir${pathsep}$indexfile");
+	print $OUT  "-- Tables
+USE DS3;
+CREATE FULLTEXT INDEX IX_PROD_ACTOR$k ON PRODUCTS$k
   (
   ACTOR
   );
@@ -88,72 +117,49 @@ CREATE FULLTEXT INDEX IX_PROD_TITLE$k ON PRODUCTS$k
   TITLE
   );
 
-CREATE INDEX IX_PROD_SPECIAL$k ON PRODUCTS$k 
+CREATE INDEX IX_PROD_SPECIAL$k ON PRODUCTS$k
   (
   SPECIAL
   );
 
+CREATE INDEX IX_PROD_PRODID_COMMON$k ON PRODUCTS$k
+  (
+  COMMON_PROD_ID
+  );
+\n";
+  close $OUT;
+  sleep(1);
+  print ("mysql -h $mysqltarget -u web --password=web < $mysql_targetdir${pathsep}$indexfile\n");
+  system ("mysql -h $mysqltarget -u web --password=web < $mysql_targetdir${pathsep}$indexfile $mybackground");
+  }
+
+$indexfile="mysqlds35_create_membership_indexes.sql";
+foreach my $k (1 .. $numberofstores){
+	open (my $OUT, ">$mysql_targetdir${pathsep}$indexfile") || die("Can't open $mysql_targetdir${pathsep}$indexfile");
+	print $OUT  "-- Tables
+USE DS3;
 ALTER TABLE MEMBERSHIP$k
   ADD CONSTRAINT FK_MEMBERSHIP_CUSTID$k FOREIGN KEY (CUSTOMERID)
   REFERENCES CUSTOMERS$k (CUSTOMERID)
   ON DELETE CASCADE
   ;
+\n";
+  close $OUT;
+  sleep(1);
+  print ("mysql -h $mysqltarget -u web --password=web < $mysql_targetdir${pathsep}$indexfile\n");
+  system ("mysql -h $mysqltarget -u web --password=web < $mysql_targetdir${pathsep}$indexfile $mybackground");
+  }
 
-ALTER TABLE REVIEWS$k
-  ADD CONSTRAINT FK_REVIEW_CUSTOMERID$k FOREIGN KEY (CUSTOMERID)
-  REFERENCES CUSTOMERS$k (CUSTOMERID)
-  ON DELETE CASCADE
-  ;
-
-CREATE INDEX IX_REVIEWS_PROD_ID$k ON REVIEWS$k
-  (
-  PROD_ID
-  );
-
-CREATE INDEX IX_REVIEWS_STARS$k on REVIEWS$k
-  (
-  STARS
-  );
+$indexfile="mysqlds35_create_review_indexes.sql";
+foreach my $k (1 .. $numberofstores){
+	open (my $OUT, ">$mysql_targetdir${pathsep}$indexfile") || die("Can't open $mysql_targetdir${pathsep}$indexfile");
+	print $OUT  "-- Tables
+USE DS3;
 
 CREATE INDEX IX_REVIEWS_PRODSTARS$k on REVIEWS$k
   (
   PROD_ID,
   STARS
-  );
-
-ALTER TABLE REVIEWS_HELPFULNESS$k
-  ADD CONSTRAINT FK_REVIEW_ID$k FOREIGN KEY (REVIEW_ID)
-  REFERENCES REVIEWS$k (REVIEW_ID)
-  ON DELETE CASCADE
-  ;
-
-CREATE INDEX IX_REVIEWS_HELP_REVID$k on REVIEWS_HELPFULNESS$k
-  (
-  REVIEW_ID
-  );
-
-CREATE INDEX IX_CUST_HIST_CUSTOMERID_PRODID$k ON CUST_HIST$k
-   (
-   CUSTOMERID ,
-   PROD_ID
-   );
-
-
-CREATE INDEX IX_REVIEWS_HELP_CUSTID$k on REVIEWS_HELPFULNESS$k
-  (
-  CUSTOMERID
-  );
-
-CREATE INDEX IX_PROD_PRODID_COMMON$k ON PRODUCTS$k
-  (
-  PROD_ID,
-  COMMON_PROD_ID
-  );
-
-CREATE INDEX IX_REVIEW_HELP_ID_HELPID$k ON REVIEWS_HELPFULNESS$k
-  (
-  REVIEW_ID,
-  REVIEWS_HELPFULNESS_ID
   );
 
 CREATE INDEX IX_REVIEWS_PRODID_REVID_DATE$k ON REVIEWS$k
@@ -163,6 +169,46 @@ CREATE INDEX IX_REVIEWS_PRODID_REVID_DATE$k ON REVIEWS$k
   REVIEW_DATE
   );
 
+CREATE INDEX idx_reviews_helpfulness$k ON REVIEWS$k
+  (
+  total_helpfulness
+  );
+
+ALTER TABLE REVIEWS$k
+  ADD CONSTRAINT FK_REVIEW_CUSTOMERID$k FOREIGN KEY (CUSTOMERID)
+  REFERENCES CUSTOMERS$k (CUSTOMERID)
+  ON DELETE CASCADE
+  ;
+
+\n";
+  close $OUT;
+  sleep(1);
+  print ("mysql -h $mysqltarget -u web --password=web < $mysql_targetdir${pathsep}$indexfile\n");
+  system ("mysql -h $mysqltarget -u web --password=web < $mysql_targetdir${pathsep}$indexfile $mybackground");
+  }
+
+$indexfile="mysqlds35_create_review_helpfulness_indexes.sql";
+foreach my $k (1 .. $numberofstores){
+	open (my $OUT, ">$mysql_targetdir${pathsep}$indexfile") || die("Can't open $mysql_targetdir${pathsep}$indexfile");
+	print $OUT  "-- Tables
+USE DS3;
+
+CREATE INDEX IX_REVIEWS_HELP_CUSTID$k on REVIEWS_HELPFULNESS$k
+  (
+  CUSTOMERID
+  );
+
+CREATE INDEX IX_REVIEW_HELP_ID_HELPID$k ON REVIEWS_HELPFULNESS$k
+  (
+  REVIEW_ID,
+  REVIEWS_HELPFULNESS_ID
+  );
+
+ALTER TABLE REVIEWS_HELPFULNESS$k
+  ADD CONSTRAINT FK_REVIEW_ID$k FOREIGN KEY (REVIEW_ID)
+  REFERENCES REVIEWS$k (REVIEW_ID)
+  ON DELETE CASCADE
+  ;
 
 CREATE INDEX IX_REORDER_PRODID$k on REORDER$k
   (
@@ -171,7 +217,6 @@ CREATE INDEX IX_REORDER_PRODID$k on REORDER$k
 \n";
   close $OUT;
   sleep(1);
-  print ("mysql -h $mysqltarget -u web --password=web < $mysql_targetdir${pathsep}mysqlds35_createindexes.sql\n");
-  system ("mysql -h $mysqltarget -u web --password=web < $mysql_targetdir${pathsep}mysqlds35_createindexes.sql");
-  #system ("del $mysql_targetdir${pathsep}mysqlds35_createindexes.sql");
+  print ("mysql -h $mysqltarget -u web --password=web < $mysql_targetdir${pathsep}$indexfile\n");
+  system ("mysql -h $mysqltarget -u web --password=web < $mysql_targetdir${pathsep}$indexfile");
   }
