c54d78dd23082a5c765c1b4d4f7b86db9f8a89bb
dyeisley <dyeisley@redhat.com>
2026-07-21 14:22:10 -0400

    Fix syntax line in sqlserver load script. No help from claude on this one.

--- sqlserver/sqlserver_ds_create_all.sh
+++ sqlserver/sqlserver_ds_create_all.sh
@@ -1,5 +1,5 @@
 # start in ./ds36/sqlserver
-# Syntax is: remote_sqlserver_ds_create_all_concurrent.sh <sqlserverdbtarget> <number of stores> <password> <use_vectors>
+# Syntax is: ./sqlserver_ds_create_all.sh <target> <number of stores> <password> <use_vectors>
 # Assumes sqlcmd is in PATH.
 
 TARGET=${1:-`hostname`}
